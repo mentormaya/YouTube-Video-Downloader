f50ae835e005092fe935c509a5b968ff07b7d42a
Ajay Singh <ajay@mentormaya.com>
2022-08-29 17:14:38 +0545

    code well managed

---
+++ libs/YouTube.py
@@ -0,0 +1,61 @@
+import json
+import threading
+from dotenv import dotenv_values
+from pytube import YouTube, Playlist
+
+#-----------------------------------------------
+# local Library
+#-----------------------------------------------
+from libs.PythonJson import PythonJson as Pjson     #could be used from dotmap import DotMap
+
+config = Pjson(dotenv_values(".env"))
+
+class Downloader():
+    def __init__(self):
+        self.config = config
+        self.url = str(self.config.URL)
+        self.output_folder = str(self.config.OUTPUT_FOLDER)
+        self.resolution = 'highest'
+        self.multi_threading = bool(self.config.MULTITHREADING) if self.config.MULTITHREADING else False
+    
+    def get(self, url = None):
+        if url is not None:
+            self.url = str(url)
+        self.yt = Playlist(self.url)
+        self.total_videos = len(self.yt.video_urls)
+        self.title = self.yt.title
+        
+    def select_resolution(self, video):
+        for index, stream in enumerate(video.streams):
+            print(f'{index}. {stream.mime_type.split("/")[1]} {stream.resolution}')
+        res = input("Choose the quality?")
+        print(f'Video {video.title} ({video.streams[res].mime_type.split("/")[1]} {video.streams[res].resolution}) is selected...')
+        return video.streams[res]
+            
+    def get_highest_resolution(self, video):
+        print(f'Highest Resolution: {video.streams.get_highest_resolution().resolution} ({video.streams.get_highest_resolution().mime_type.split("/")[1]}) for {video.title} is selected...')
+        return video.streams.get_highest_resolution()
+
+    def get_stream(self, video, resolution):
+        if resolution == 'highest':
+            return self.get_highest_resolution(video)
+        if resolution == 'select':
+            return self.select_resolution(video)
+        #TODO for other resolutions
+
+    def download(self, out_path = None, resolution = 'highest'):
+        if out_path is not None:
+            self.output_folder = str(out_path)
+        self.resolution = resolution
+        if self.total_videos > 1:
+            full_out_location = self.output_folder + "/" + self.title + "/"
+        else:
+            full_out_location = self.output_folder + "/"
+        if self.multi_threading:
+            print('Multi Threading On...')
+        # for video in self.yt.videos:
+        #     self.save(video, full_out_location)
+        print(f"All the downloads Completed Successfully and Saved to {full_out_location}")
+    
+    def save(self, video, path):
+        self.get_stream(video, self.resolution).download(path)
--- main.py
+++ main.py
@@ -1,15 +1,73 @@
-from libs.PythonJson import PythonJson as Pjson
-from dotenv import dotenv_values
 from tkinter import *
+from tkinter import ttk
+from dotenv import dotenv_values
+
+#-----------------------------------------------
+# local Library
+#-----------------------------------------------
+from libs.PythonJson import PythonJson as Pjson     #could be used from dotmap import DotMap
+from libs.YouTube import Downloader
 
 config = Pjson(dotenv_values(".env"))
 class App():
     def __init__(self):
-        self.app = Tk()
+        self.main_window = Tk()
         self.config = config
+        self.downloader = Downloader()
+        self.setup()
+        self.main_window.mainloop()
+    
+    def setup(self):
+        self.setupUI()
+        self.center()
+        self.main_window.bind("<Key>", self.handle_keypress)
+        self.download()
+    
+    def setupUI(self):
+        self.main_window.geometry(self.config.APP_SIZE)
+        self.main_window.attributes('-alpha', self.config.ALPHA)
+        self.main_window.title(self.config.APP_NAME + " " + self.config.APP_VERSION)
+        main_frame = ttk.Frame()
+        main_frame.pack()
+        ttk.Label(
+            master=main_frame, 
+            text=   "Welcome to YouTube Downloader",
+            font=   ("Monaco", 20, "bold")
+        ).pack(padx=10, pady=10)
+    
+    def download(self):
+        url = "https://www.youtube.com/watch?v=I2PsRRgRKto"
+        self.downloader.get()
+        self.downloader.download()
         
     
-
+    def center(self):
+        """
+        centers a tkinter window
+        :param win: the main window or Toplevel window to center
+        """
+        self.main_window.update_idletasks()
+        width = self.main_window.winfo_width()
+        frm_width = self.main_window.winfo_rootx() - self.main_window.winfo_x()
+        win_width = width + 2 * frm_width
+        height = self.main_window.winfo_height()
+        titlebar_height = self.main_window.winfo_rooty() - self.main_window.winfo_y()
+        win_height = height + titlebar_height + frm_width
+        x = self.main_window.winfo_screenwidth() // 2 - win_width // 2
+        y = self.main_window.winfo_screenheight() // 2 - win_height // 2
+        self.main_window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+        self.main_window.deiconify()
+    
+    def handle_keypress(self, event):
+        if event.keysym == 'Escape':
+            print('Escape Key Pressed: App Closing...')
+            self.quitApp()
+            return
+        else:
+            print(event)
+    
+    def quitApp(self):
+        self.main_window.destroy()
 
 if __name__ == '__main__':
     app = App()
