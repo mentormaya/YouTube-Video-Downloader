30fc1b4c778e597dad7d60adca3ac71ccdc6b21a
Ajay Singh <ajay@mentormaya.com>
2022-09-05 16:10:43 +0545

    script updated

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,4 +1,4 @@
 echo off
 echo "Building the project..."
-pyinstaller --onefile --windowed --icon "assets/images/YouTube.ico" --splash "assets/images/YouTube-icon.png" --name "YouTube Video Downloader" --clean --add-data "assets/*;assets/" --add-data ".env;." --add-data ".venv/Lib/site-packages/nepali_datetime/data/calendar_bs.csv;nepali_datetime/data/" main.py
+pyinstaller --onefile --windowed --icon "assets/images/YouTube.ico" --splash "assets/images/YouTube-icon.png" --name "YouTube Video Downloader" --clean --add-data "assets/*.*;assets/" --add-data ".env;." --add-data ".venv/Lib/site-packages/nepali_datetime/data/calendar_bs.csv;nepali_datetime/data/" main.py
 echo "Exe created Successfully! 💖"
