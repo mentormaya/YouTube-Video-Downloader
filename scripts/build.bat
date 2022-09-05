c44f673642be9f66670a41a1cf8fdf8e36325247
Ajay Singh <ajay@mentormaya.com>
2022-09-05 16:51:32 +0545

    Build Script updated

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,4 +1,5 @@
 echo off
 echo "Building the project..."
-pyinstaller --onefile --windowed --icon "assets/images/YouTube.ico" --splash "assets/images/YouTube-icon.png" --name "YouTube Video Downloader" --clean --add-data "assets/*.*;assets/" --add-data ".env;." --add-data ".venv/Lib/site-packages/nepali_datetime/data/calendar_bs.csv;nepali_datetime/data/" main.py
-echo "Exe created Successfully! 💖"
+pyinstaller --onefile --windowed --icon "assets/images/YouTube.ico" --splash "assets/images/YouTube-icon.png" --name "YouTube Video Downloader" --clean --resource "assets" --add-data ".env;." --add-data ".venv/Lib/site-packages/nepali_datetime/data/calendar_bs.csv;nepali_datetime/data/" main.py
+echo "EXE Creation Completed Successfully!"
+::pyinstaller --noconfirm --onefile --windowed --icon "D:/Ajay/GitHub/YouTube-Video-Downloader/assets/images/YouTube.ico" --name "YouTube Video Downloader" --clean --log-level "DEBUG" --splash "D:/Ajay/GitHub/YouTube-Video-Downloader/assets/images/YouTube-icon.png" --add-data ".env;." --add-data "D:/Ajay/GitHub/YouTube-Video-Downloader/assets;assets/" --add-data "D:/Ajay/GitHub/YouTube-Video-Downloader/.venv/Lib/site-packages/nepali_datetime/data/calendar_bs.csv;nepali_datetime/data"  "D:/Ajay/GitHub/YouTube-Video-Downloader/main.py"
