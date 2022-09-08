bfad749b0bf6c9d851d8179d60f76e9f41e54149
Ajay Singh <ajay@mentormaya.com>
2022-09-08 09:32:25 +0545

    build script for macOs is added

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+echo off
+echo "Building the project..."
+pyinstaller\
+    --onefile\
+    --windowed\
+    --icon "assets/images/YouTube.ico"\
+    --splash "assets/images/YouTube-icon.png"\
+    --name "YouTube Video Downloader"\
+    --clean\
+    --add-data "assets/images/*.png:assets/images/"\
+    --add-data "assets/images/*.ico:assets/images/"\
+    --add-data "assets/contents/*.txt:assets/contents/"\
+    --add-data ".env:."\
+    --add-data ".venv/Lib/site-packages/nepali_datetime/data/calendar_bs.csv:nepali_datetime/data/"\
+ main.py
+echo "EXE Creation Completed Successfully!"
