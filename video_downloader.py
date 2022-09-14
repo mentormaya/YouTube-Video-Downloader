7fa806adb5771f145f0d8cbbb2aec88881a3b6ed
Ajay Singh <ajay@mentormaya.com>
2022-09-14 20:03:17 +0545

    updated video downloader.py

--- video_downloader.py
+++ video_downloader.py
@@ -2,27 +2,38 @@
 
 import requests
 from pprint import pprint
-import m3u8_To_MP4 as mp4
+import m3u8
+
+def _get_m3u8_obj_by_uri(m3u8_uri):
+        try:
+            m3u8_obj = m3u8.load(uri=m3u8_uri)
+        except Exception as exc:
+            pprint('failed to load m3u8 file,reason is {}'.format(exc))
+            raise Exception('FAILED TO LOAD M3U8 FILE!')
+
+        return m3u8_obj
 
 URL = 'https://prmovies.com/the-batman-2022-hindi-dubbed-Watch-online-on-prmovies/'
-
 
 s = requests.Session()
 
 r = s.get(URL)
 
-print(r.text)
+m3u8_file = 'https://trxjhsdv.ydc1wes.me/hls2/01/00003/zvtzllxzil6u_l/master.m3u8?t=E9EA3mDipr13zZRMd16a4Smcv3JM9Injnwk4gxK34Hw&s=1663163892&e=21600&f=18545&i=0.0&sp=0'
 
-# m3u8_file = 'https://leave.ydc1wes.me/hls2/01/00003/c9woiuznhaa2_,l,o,.urlset/master.m3u8?t=4Hcb__PV33j1oMqazeo6WyKdJ3weVcANfOntcnXZl5c&s=1663127503&e=21600&f=16229&i=0.0&sp=0'
-
-# print('Get m3u8 file...')
+print('Get m3u8 file...')
 
 customized_http_header=dict()
-customized_http_header['Referer']=URL
+customized_http_header['Referer'] = URL
 
-print("Downloaing...")
+print("Downloading...")
 
-# r = s.get(m3u8_file)
+r = s.get(m3u8_file)
+
+m3u8_obj = _get_m3u8_obj_by_uri(m3u8_file)
+
+pprint(m3u8_obj.__dict__)
+
 # playlist = mp4.multithread_download(m3u8_file,customized_http_header=customized_http_header)
 
 # pprint(r.__dict__)
