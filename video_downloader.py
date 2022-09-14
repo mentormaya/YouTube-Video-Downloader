6dc68f4ac9121f809c89cb3ce3beb7d90fda86e8
Ajay Singh <ajay@mentormaya.com>
2022-09-14 09:44:11 +0545

    video downloader updated

--- video_downloader.py
+++ video_downloader.py
@@ -3,10 +3,15 @@
 import requests
 from pprint import pprint
 
-URL = 'https://cms.ntc.net.np'
+URL = 'https://prmovies.com/the-batman-2022-hindi-dubbed-Watch-online-on-prmovies/'
+
 
 s = requests.Session()
 
 r = s.get(URL)
 
-pprint(r.__dict__)
+m3u8_file = 'https://leave.ydc1wes.me/hls2/01/00003/c9woiuznhaa2_,l,o,.urlset/master.m3u8?t=4Hcb__PV33j1oMqazeo6WyKdJ3weVcANfOntcnXZl5c&s=1663127503&e=21600&f=16229&i=0.0&sp=0'
+
+r = s.get(m3u8_file)
+
+pprint(r.text)
