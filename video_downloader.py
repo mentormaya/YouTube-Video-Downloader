15119520187d124e438c27bb6b6d9a02382921f2
Ajay Singh <ajay@mentormaya.com>
2022-09-15 09:21:42 +0545

    merged

--- video_downloader.py
+++ video_downloader.py
@@ -19,20 +19,16 @@ s = requests.Session()
 
 r = s.get(URL)
 
-<<<<<<< HEAD
 pprint(r.text)
-=======
+
 m3u8_file = 'https://trxjhsdv.ydc1wes.me/hls2/01/00003/zvtzllxzil6u_l/master.m3u8?t=E9EA3mDipr13zZRMd16a4Smcv3JM9Injnwk4gxK34Hw&s=1663163892&e=21600&f=18545&i=0.0&sp=0'
->>>>>>> 7fa806adb5771f145f0d8cbbb2aec88881a3b6ed
+
 
 print('Get m3u8 file...')
 
 customized_http_header=dict()
-<<<<<<< HEAD
+
 customized_http_header['Referer'] = 'https://speedostream.com/'
-=======
-customized_http_header['Referer'] = URL
->>>>>>> 7fa806adb5771f145f0d8cbbb2aec88881a3b6ed
 
 print("Downloading...")
 
@@ -42,13 +38,10 @@ m3u8_obj = _get_m3u8_obj_by_uri(m3u8_file)
 
 pprint(m3u8_obj.__dict__)
 
-<<<<<<< HEAD
 # r = s.get(m3u8_file)
 
 # pprint(r.text)
 
-=======
->>>>>>> 7fa806adb5771f145f0d8cbbb2aec88881a3b6ed
 # playlist = mp4.multithread_download(m3u8_file,customized_http_header=customized_http_header)
 
 # pprint(r.__dict__)
