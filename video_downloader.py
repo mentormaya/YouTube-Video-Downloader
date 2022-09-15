4ff6eb1637ef14957247052925b1916cd2e0f37a
Ajay Singh <ajay@mentormaya.com>
2022-09-15 09:19:33 +0545

    referer url added

--- video_downloader.py
+++ video_downloader.py
@@ -11,18 +11,21 @@ s = requests.Session()
 
 r = s.get(URL)
 
-print(r.text)
+pprint(r.text)
 
 # m3u8_file = 'https://leave.ydc1wes.me/hls2/01/00003/c9woiuznhaa2_,l,o,.urlset/master.m3u8?t=4Hcb__PV33j1oMqazeo6WyKdJ3weVcANfOntcnXZl5c&s=1663127503&e=21600&f=16229&i=0.0&sp=0'
 
 # print('Get m3u8 file...')
 
 customized_http_header=dict()
-customized_http_header['Referer']=URL
+customized_http_header['Referer'] = 'https://speedostream.com/'
 
 print("Downloaing...")
 
 # r = s.get(m3u8_file)
+
+# pprint(r.text)
+
 # playlist = mp4.multithread_download(m3u8_file,customized_http_header=customized_http_header)
 
 # pprint(r.__dict__)
