b98d03811d290d27b9da7860c4a6992226a87cfe
Ajay Singh <ajay@mentormaya.com>
2022-09-17 16:15:16 +0545

    deobs js progressed

--- video_downloader.py
+++ video_downloader.py
@@ -1,10 +1,19 @@
 # /usr/bin/python3
-
+import re
+import time
+import math
 import m3u8
 import requests
 from pprint import pprint
-import m3u8_To_MP4 as mp4
+from bs4 import BeautifulSoup
+# import m3u8_To_MP4 as mp4
 
+
+offset_pattern = r'offset=(\d+)'
+
+window_pattern = r'"window\[\'(.+)\']='
+
+w_params_pattern = r'{r:\'(.+)\',m:\'(.+)\',s:(\[.+\]),u:\'(.+)\'}'
 
 def _get_m3u8_obj_by_uri(m3u8_uri):
         try:
@@ -15,43 +24,43 @@ def _get_m3u8_obj_by_uri(m3u8_uri):
 
         return m3u8_obj
 
-def download(m3u8_file):
-    customized_http_header=dict()
+# def download(m3u8_file):
+#     customized_http_header=dict()
 
-    customized_http_header['Referer'] = 'https://speedostream.com/'
+#     customized_http_header['Referer'] = 'https://speedostream.com/'
 
-    print('Getting m3u8 file...')
+#     print('Getting m3u8 file...')
 
-    m3u8_obj = _get_m3u8_obj_by_uri(m3u8_file)
+#     m3u8_obj = _get_m3u8_obj_by_uri(m3u8_file)
 
-    playlists = m3u8_obj.data['playlists']
+#     playlists = m3u8_obj.data['playlists']
 
-    bandwidth = 0
+#     bandwidth = 0
 
-    stream = {}
+#     stream = {}
 
-    for playlist in playlists:
-        if bandwidth < playlist['stream_info']['bandwidth']:
-            stream = playlist['stream_info']
-            stream['url'] = playlist['uri']
+#     for playlist in playlists:
+#         if bandwidth < playlist['stream_info']['bandwidth']:
+#             stream = playlist['stream_info']
+#             stream['url'] = playlist['uri']
 
-    print(f'Selected Best Stream: {stream["resolution"]} ({stream["frame_rate"]} fps)')
+#     print(f'Selected Best Stream: {stream["resolution"]} ({stream["frame_rate"]} fps)')
 
-    print("Fetching Stream...")
+#     print("Fetching Stream...")
 
-    stream_data = _get_m3u8_obj_by_uri(stream['url'])
+#     stream_data = _get_m3u8_obj_by_uri(stream['url'])
 
-    files = stream_data.files
+#     files = stream_data.files
 
-    print(f'Total {len(files)} segments found!')
+#     print(f'Total {len(files)} segments found!')
 
-    mp4.multithread_download(
-        stream['url'], customized_http_header=customized_http_header, 
-        mp4_file_dir='./Downloads/', 
-        mp4_file_name=f'{fname}.mp4'
-    )
+#     mp4.multithread_download(
+#         stream['url'], customized_http_header=customized_http_header, 
+#         mp4_file_dir='./Downloads/', 
+#         mp4_file_name=f'{fname}.mp4'
+#     )
 
-URL = 'https://prmovies.com/the-batman-2022-hindi-dubbed-Watch-online-on-prmovies/'
+URL = 'https://prmovies.com/jogi-2022-punjabi-Watch-online-on-prmovies/'
 
 HEADERS = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -74,11 +83,36 @@ HEADERS = {
 
 s = requests.Session()
 
-# r = s.get(URL, headers=HEADERS)
+w = s.get(URL, headers=HEADERS)
 
-r = s.get("https://speedostream.com/embed-uq5bejn8cnxs.html")
+w_soup = BeautifulSoup(w.text, 'html.parser')
 
-print(r.content)
+iframes = w_soup.find_all('iframe')
+
+if_links = [iframe['src'] for iframe in iframes]
+
+r = s.get(if_links[0])
+
+# r_content = r.text
+
+offset = int(re.search(offset_pattern, r.text).groups()[0])
+
+w_params = re.search(w_params_pattern, r.text)
+
+now = time.time()
+
+params = dict(
+    ts = math.floor(now) - math.floor(now%offset),
+    window_key = re.search(window_pattern, r.text).groups()[0],
+    window_params = dict(
+        r = w_params.groups()[0],
+        m = w_params.groups()[1],
+        s = w_params.groups()[2],
+        u = w_params.groups()[3]
+    )
+)
+
+pprint(params)
 
 fname = 'Film'
 
