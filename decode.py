799818e2adaef500687c1502d59c1ca78ff78aaa
Ajay Singh <ajay@mentormaya.com>
2022-09-20 17:13:30 +0545

    got the m3u8 file from the links

--- decode.py
+++ decode.py
@@ -1,33 +1,74 @@
-#/usr/bin/python3
-
 import re
 from pprint import pprint
+from requests import Session
 
-word_list_pattern = r"var sjana = (\[.*\])"
+s = Session()
 
-get_word_func_call_pattern = r"get_word\((\d+)\)"
+HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept': '-encoding: gzip, deflate, br',
+    'accept': '-language: en',
+    'cookie': 'domain-alert=1',
+    'dnt': '1',
+    'referer': 'https://prmovies.com/',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': "Windows",
+    'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+}
 
-js_file = open("app.min.js", "r")
+url = 'https://sbplay2.xyz/e/m5cod5c7ole6?caption_1=https://msubload.com/sub/the-good-doctor-season-1-episode-2-mount-rushmore/the-good-doctor-season-1-episode-2-mount-rushmore.vtt&sub_1=English'
 
-js_content = js_file.read()
+domain_pattern = r"(^https?://[A-Za-z_0-9-]+\.[A-Za-z_0-9-]+)"
 
-word_list = re.search(word_list_pattern, js_content).groups()[0].strip('"]["').split('", "')
+word_list = None
 
-indices = re.findall(get_word_func_call_pattern, js_content)
+def get_word(index):
+    index = index - 119
+    if word_list is not None:
+        if index in range(len(word_list)):
+            return word_list[index]
+        else:
+            return [f'Index not in range {index}/{len(word_list)}']
+    else:
+        return ['No Word List Defined!']
 
-new_js_content = js_content
+def hex_to_decimal(hex):
+    decimals = []
+    for value in hex.split(','):
+        decimals.append(int(value, 16))
+    pprint(f'Decimals for {hex} is {decimals}')
+    return decimals
+
+js_url = re.search(domain_pattern, url).groups()[0] + "/js/app.min.3.js?v=2"
+
+content = s.get(url = js_url, headers = HEADERS)
+
+with open("latest_app.js", 'w') as f:
+    f.write(content.text)
+
+word_list_pattern = r'var\s?\w+\s?=(\[.*?\]);'
+
+word_list = re.search(word_list_pattern, content.text).groups()[0].strip("']['")
+word_list = word_list.split("','")
 
 total_words = len(word_list)
 
-for index in indices:
-    index = int(index)
-    if index > total_words or index < 119:
-        # print(f'range of index for {index - 119}/{total_words} is exceeded skipping')
-        continue
-    # print(f'replacing {index} call')
-    new_js_content = re.sub(get_word_func_call_pattern, f'"{word_list[index - 119]}"', new_js_content, 1)
+print(total_words)
 
-with open('new_app.js', 'w') as f:
-    f.write(new_js_content)
+# sources_pattern = r"'MNLSf':_\w+\((.*?)\)\+_\w+\((.*?)\)\+\'(.+?)\'"
 
-print('New file created check!')
+# sources_options = re.search(sources_pattern, content.text).groups()
+
+# for option in sources_options:
+#     if '0x' not in option:
+#         continue
+#     else:
+#         decimals = hex_to_decimal(option)
+
+print(get_word(227) + get_word(654) + "/")
--- test.py
+++ test.py
@@ -1,43 +1,96 @@
+# 513834386a6652764e4d3241 7c7c 6d35636f643563376f6c6536 7c7c 4f614d593335314674774a6a 7c7c 73747265616d7362
+# 694b7864377934576b567278 7c7c 6d35636f643563376f6c6536 7c7c 43657139596b64494441564b 7c7c 73747265616d7362
+# 545a487965424e346e566b72 7c7c 6d35636f643563376f6c6536 7c7c 6a614f487578676472773973 7c7c 73747265616d7362
+# 356c75445569787967553868 7c7c 6d35636f643563376f6c6536 7c7c 415731736b484a597769576b 7c7c 73747265616d7362
+
+# 324152664b3073304563435a 7c7c 6d35636f643563376f6c6536 7c7c 7a42314c5630667068764841 7c7c 73747265616d7362
+# 6650394465783035726e3567 7c7c 6d35636f643563376f6c6536 7c7c 4b417a735a42525545596f42 7c7c 73747265616d7362
 import re
+import random
+import json
 from pprint import pprint
 from requests import Session
 
 s = Session()
 
-HEADERS = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept': '-encoding: gzip, deflate, br',
-    'accept': '-language: en',
-    'cookie': 'domain-alert=1',
+url = 'https://sbplay2.xyz/e/m5cod5c7ole6?caption_1=https://msubload.com/sub/the-good-doctor-season-1-episode-3-mount-rushmore/the-good-doctor-season-1-episode-3-mount-rushmore.vtt&sub_1=English'
+
+domain_pattern = r"(https?://[A-Za-z._\-0-9]+)"
+
+def get_domain(url):
+    return re.search(domain_pattern, url).groups()[0]
+
+M3U8_LINK_HEADERS = {
+    'accept': 'application/json, text/plain, */*',
+    # 'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en',
+    'cookie': 'lang=1',
     'dnt': '1',
-    'referer': 'https://prmovies.com/',
+    'referer': url,
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': "Windows",
     'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
+    'watchsb': 'sbstream',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
 }
 
-url = 'https://sbplay2.xyz/e/m5cod5c7ole6?caption_1=https://msubload.com/sub/the-good-doctor-season-1-episode-2-mount-rushmore/the-good-doctor-season-1-episode-2-mount-rushmore.vtt&sub_1=English'
 
-domain_pattern = r"(^https?://[A-Za-z_0-9-]+\.[A-Za-z_0-9-]+)"
+def hex_to_decimal(hex):
+    decimals = []
+    for value in hex.split(','):
+        decimals.append(int(value, 16))
+    pprint(f'Decimals for {hex} is {decimals}')
+    return decimals
 
-js_url = re.search(domain_pattern, url).groups()[0] + "/js/app.min.3.js?v=2"
+def get_random_id(length):
+    chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    id = ''.join(random.choice(chars) for i in range(length))
+    return id
 
-content = s.get(url = js_url, headers = HEADERS)
+def generate_link(path):
+    return get_domain(url) + path
 
-with open("latest_app.js", 'w') as f:
-    f.write(content.text)
+def generate_url():
+    m5cod5c7ole6 = '6d35636f643563376f6c6536'
+    streamsb = '73747265616d7362'
+    sep = '7c7c'
+    return generate_link('/sources48/') + sep.join([get_random_id(24), m5cod5c7ole6, get_random_id(24), streamsb])
 
-word_list_pattern = r'var\s?\w+\s?=(\[.*?\]);'
+link = generate_url()
 
-word_list = re.search(word_list_pattern, content.text).groups()[0].strip("']['")
-word_list = word_list.split("','")
+print(f'getting the m3u8 link {link}')
+m3u8_link = s.get(link, headers=M3U8_LINK_HEADERS)
 
-total_words = len(word_list)
+print(f'writing the m3u8 link to file')
+with open('stream_data.json', 'w') as sjf:
+    sjf.write(m3u8_link.text)
 
-print(total_words)
+m3u8_link = json.loads(m3u8_link.text)
+
+m3u8_url = m3u8_link['stream_data']['file']
+
+M3U8_FILE_HEADERS = {
+    'accept': '*/*',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en',
+    'connection': 'keep-alive',
+    'dnt': '1',
+    'host': get_domain(m3u8_url).split('//')[1],
+    'origin': get_domain(url),
+    'referer': get_domain(url) + "/",
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': "Windows",
+    'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+}
+
+print(f'getting the m3u8 file from {m3u8_url}')
+m3u8_data = s.get(m3u8_url, headers=M3U8_FILE_HEADERS)
+
+pprint(m3u8_data.text)
